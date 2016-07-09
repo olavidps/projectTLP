cb070f5742c70d5297c51a2663a4467000820c70
Olavid <olavidps@gmail.com>
2016-07-09 18:39:04 -0500

    Uso de header, footer y nav

---
+++ footer.php
@@ -0,0 +1,84 @@
+  <footer class="row">
+        <div class="row m0 topFooter">
+            <div class="container line1">
+                <div class="row footFeatures">
+                    <div class="col-sm-4 col-md-4 col-xs-4 footFeature">
+                        <div class="media">
+                            <div class="media-left icon"><img src="img/icon/car3.png" alt=""></div>
+                            <div class="media-body texts">
+                                <h4>Envío nacional</h4>
+                                <h2>24/7</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-md-4 col-xs-4 footFeature">
+                        <div class="media m0">
+                            <div class="media-left icon"><img src="img/icon/tel24-7_2.png" alt=""></div>
+                            <div class="media-body texts">
+                                <h4>Atención de 8am - 8pm</h4>
+                                <h2>(044) 232377</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 col-md-4 col-xs-4 footFeature">
+                        <div class="media m0">
+                            <div class="media-left icon"><img src="img/icon/shopping-bag2.png" alt=""></div>
+                            <div class="media-body texts">
+                                <h4>conoce nuestras</h4>
+                                <h2>Ofertas</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row m0 copyRight">
+            <div class="container">
+                <div class="row">
+                    <div class="fleft">&copy; 2016 <a href="index.html">Proyecto TLP</a> Todos los derechos reservados.</div>
+                    <ul class="nav nav-pills fright">
+                        <li><a href="index.html">Inicio</a></li>
+                        <li><a href="about.html">Productos</a></li>
+                        <li><a href="blog.html">Contacto</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    <!--jQuery, Bootstrap and other vendor JS-->
+    
+    <!--jQuery-->
+    <script src="js/jquery-2.1.3.min.js"></script>
+    
+    <!--Google Maps-->
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    
+    <!--Bootstrap JS-->
+    <script src="js/bootstrap.min.js"></script>
+    
+    <!--Nice Scroll-->
+    <script src="js/nicescroll/jquery.nicescroll.js"></script>
+        
+    <!--Lightshot-->
+    <script src="js/lightbox.min.js"></script>
+
+    <!--Counter Up-->
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    
+    <!--Owl Carousel-->
+    <script src="js/owl.carousel.min.js"></script>
+    
+    <!--Isotope-->
+    <script src="js/isotope-custom.js"></script>
+    
+    <!--Rating-->
+    <script src="js/bootstrap-rating.min.js"></script>
+    
+    <!--FlexSlider-->
+    <script src="js/jquery.flexslider-min.js"></script>
+    
+    <!--Strella JS-->
+    <script src="js/furniture.js"></script>
---
+++ header.php
@@ -0,0 +1,41 @@
+<!--Favicons-->
+    <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    
+    <!--Bootstrap and Other Vendors-->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/carousel.css">    
+    
+    <link rel="stylesheet" type="text/css" href="css/lightbox.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/flexslider.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-rating.css">
+    
+    <!--Fonts-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800|Montserrat:400,700' rel='stylesheet' type='text/css'>
+    
+    <!--Mechanic Styles-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+    <script src="https://use.fontawesome.com/ea41b501c2.js"></script>
+    
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
